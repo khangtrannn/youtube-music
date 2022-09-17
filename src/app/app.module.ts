ef5b9d72cf4fab4a49f1ad49832443f7141103da
Khang Tran <khangtrann8198@gmail.com>
2022-09-17 15:17:00 +0700

    Update structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/video-card/video-card.component';
+import { CardComponent } from './share/components/video-card/video-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
