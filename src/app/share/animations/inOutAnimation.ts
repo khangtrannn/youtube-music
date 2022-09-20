f976ccd99b6859fc1bee5cbdc72d7a4fbd40f894
Khang Tran <khangtrann8198@gmail.com>
2022-09-20 20:45:03 +0700

    Fix background animation

--- src/app/share/animations/inOutAnimation.ts
+++ src/app/share/animations/inOutAnimation.ts
@@ -1,6 +1,6 @@
 import { animate, style, transition, trigger } from "@angular/animations";
 
 export const inOutAnimation = trigger('inOutAnimation', [
-  transition(':enter', [animate('2.5s ease-out')]),
-  transition(':leave', [animate('2.5s ease-in')]),
+  transition(':enter', [style({ opacity: 0 }), animate('1s 0.5s', style({ opacity: 1 }))]),
+  transition(':leave', [style({ opacity: 1 }), animate('1s', style({ opacity: 0 }))]),
 ]);
