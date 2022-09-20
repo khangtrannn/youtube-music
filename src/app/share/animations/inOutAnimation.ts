62b848b3be38848646dc1494d7c56ddc2a477d1e
Khang Tran <khangtrann8198@gmail.com>
2022-09-20 23:29:39 +0700

    Disable in out animation for initial view

--- src/app/share/animations/inOutAnimation.ts
+++ src/app/share/animations/inOutAnimation.ts
@@ -1,6 +1,7 @@
 import { animate, style, transition, trigger } from "@angular/animations";
 
 export const inOutAnimation = trigger('inOutAnimation', [
+  transition('void => *', []), // No animation for initial view
   transition(':enter', [style({ opacity: 0 }), animate('1s 0.5s', style({ opacity: 1 }))]),
   transition(':leave', [style({ opacity: 1 }), animate('1s', style({ opacity: 0 }))]),
 ]);
